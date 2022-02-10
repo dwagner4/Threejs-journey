aa76cf8a1fed5d747cc3124ae9c56dd70f25ce7e
Dean Wagner <dean@dfwagners.net>
2022-02-10 13:48:44 -0500

    REFACTOR: moved material out of createBall()

--- src/Experience/World/Ball.js
+++ src/Experience/World/Ball.js
@@ -10,13 +10,25 @@ export default class Ball
     this.model = null
     this.body = null
     this.debug = this.experience.debug
+    this.firstBall = true
+    this.material = new THREE.MeshStandardMaterial({
+      color: '#777777',
+      metalness: 0.3,
+      roughness: 0.4,
+    })
 
     // Debug
-    if(this.experience.debug.active) 
-    {
-      this.debugFolder = this.experience.debug.ui.addFolder('the ball')
-    }
+    // if(this.experience.debug.active && this.firstBall) 
+    // {
+    //   this.debugFolder = this.experience.debug.ui.addFolder('the ball')
+    // }
 
+    this.createBall()
+    this.firstBall = false
+  }
+
+  createBall = () => 
+  {
     this.setModel()
     this.setAnimation()
     this.setBody()
@@ -25,29 +37,31 @@ export default class Ball
 
   setModel()
   {
-    const material = new THREE.MeshStandardMaterial({
-      color: '#777777',
-      metalness: 0.3,
-      roughness: 0.4,
-    })
+    // const material = new THREE.MeshStandardMaterial({
+    //   color: '#777777',
+    //   metalness: 0.3,
+    //   roughness: 0.4,
+    // })
     this.model = new THREE.Mesh(
       new THREE.SphereGeometry(0.2, 20, 20),
-      material
+      this.material
     )
     this.model.castShadow = true
     this.model.position.set(1,2,0)
     this.experience.scene.add(this.model)
 
     // Debug
-    if(this.debug.active)
+    if(this.debug.active && this.firstBall)
     {
+      this.debugFolder = this.experience.debug.ui.addFolder('the ball')
       const colorObject = { color: '#777777' }
-      this.debugFolder.add(material, 'metalness').min(0).max(1).step(0.01)
-      this.debugFolder.add(material, 'roughness').min(0).max(1).step(0.01)
+      this.debugFolder.add(this.material, 'metalness').min(0).max(1).step(0.01)
+      this.debugFolder.add(this.material, 'roughness').min(0).max(1).step(0.01)
       this.debugFolder.addColor(colorObject, 'color')
           .onChange( () => {
-          material.color.set(colorObject.color)
+          this.material.color.set(colorObject.color)
       }) 
+      console.log(this.debug)
     }
 
   }
@@ -69,15 +83,18 @@ export default class Ball
     this.experience.physWorld.addBody(this.body)
 
     // Debug
-    if(this.debug.active)
+    if(this.debug.active && this.firstBall)
     {
       const debugObject = {
         dropBall: () => {
           console.log('ball drop')
           this.body.position.set(1,5,0)
+        },
+        newBall: () => {
+          this.createBall()
         }
       }
-      this.debugFolder.add(debugObject, 'dropBall')
+      this.debugFolder.add(debugObject, 'newBall')
     }
 
   }
